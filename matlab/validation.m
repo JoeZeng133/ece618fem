4b803a87762ebe910f7fba0158df4499ca90964c
zhan1896 <zhan1896@purdue.edu>
2018-04-09 19:35:26 -0400

    Updated some minor stuff

--- matlab/validation.m
+++ matlab/validation.m
@@ -1,4 +1,4 @@
-%% FEM Validation %% 
+%% FEM Validation %%
 
 function validation(a, b, model, mesh, V_tm, V_te, val_flag, err_flag)
 
@@ -8,16 +8,13 @@ modefunc_te = @(m, n, x, y) cos(m * pi * x / a) .* cos(n * pi * y / b);
 % tm_mode_arr=[1 1; 2 1; 3 1; 1 2; 2 2];
 % te_mode_arr=[1 0; 0 1; 2 0; 1 1; 2 1];
 
-if (val_flag==1)
-figure;
-set(gcf,'Position',[200 200 1000 500]);
-end
+
 %title('Analytic Solution for Rectangular Waveguide');
 
-%Calculate the first few modes for TE and TM cases with specified a and b 
+%Calculate the first few modes for TE and TM cases with specified a and b
 for i=1:6
     for j=1:6
-       fc_mode_te_2d_arr(i,j)=((j-1)/a)^2+((i-1)/b)^2;
+        fc_mode_te_2d_arr(i,j)=((j-1)/a)^2+((i-1)/b)^2;
     end
 end
 
@@ -36,87 +33,93 @@ te_mode_arr=te_mode_arr(2:end,:);
 
 %In the case of a/b=2, the first five TM modes are
 %TM mode1 1,1
-%TM mode2 2,1 
+%TM mode2 2,1
 %TM mode3 3,1
 %TM mode4 1,2
 %TM mode5 2,2
+
+if (val_flag==1)
+    figure;
+    set(gcf,'Position',[200 200 1000 500]);
+end
+
 for i = 1 : 5
     
-    
     modeltrue =  modefunc_tm(tm_mode_arr(i,1), tm_mode_arr(i,2), mesh.Nodes(1,:), mesh.Nodes(2, :));
+    %k = ((tm_mode_arr(i,1)*pi/a)^2 + (tm_mode_arr(i,2)*pi/b)^2) ^ 0.5;
     modeltrue_tm(:,i)=modeltrue / max(abs(modeltrue(:)));
     
     if (val_flag==1)
-    subplot(2,5,i);
-    pdeplot(model,'XYData', modeltrue_tm(:,i)')
-
-    colormap jet
-    xlabel('x')
-    ylabel('y')
-    title(['TM_{', num2str(tm_mode_arr(i,1)),num2str(tm_mode_arr(i,2)),'}']);
+        subplot(2,5,i);
+        pdeplot(model,'XYData', modeltrue_tm(:,i)')
+        
+        colormap jet
+        xlabel('x')
+        ylabel('y')
+        title(['TM_{', num2str(tm_mode_arr(i,1)),num2str(tm_mode_arr(i,2)),'}']);
     end
     
 end
 
 %In the case of a/b=2, the first five TE modes are
 %TE mode1 1,0
-%TE mode2 0,1 
+%TE mode2 0,1
 %TE mode3 2,0
 %TE mode4 1,1
 %TE mode5 2,1
 for i = 1 : 5
     
-    
     modeltrue=modefunc_te(te_mode_arr(i,1), te_mode_arr(i,2), mesh.Nodes(1,:), mesh.Nodes(2, :));
+    %k = ((te_mode_arr(i,1)*pi/a)^2 + (te_mode_arr(i,2)*pi/b)^2) ^ 0.5;
     modeltrue_te(:,i)=(modeltrue - min(modeltrue)) / (max(modeltrue) - min(modeltrue)) * 2 - 1;
     
     if (val_flag==1)
-    subplot(2,5,i+5);    
-    pdeplot(model,'XYData', modeltrue_te(:,i)')
-
-    colormap jet
-    xlabel('x')
-    ylabel('y')
-    title(['TE_{', num2str(te_mode_arr(i,1)),num2str(te_mode_arr(i,2)),'}']);
+        subplot(2,5,i+5);
+        pdeplot(model,'XYData', modeltrue_te(:,i)')
+        
+        colormap jet
+        xlabel('x')
+        ylabel('y')
+        title(['TE_{', num2str(te_mode_arr(i,1)),num2str(te_mode_arr(i,2)),'}']);
     end
 end
 
 if (err_flag==1)
-figure;
-set(gcf,'Position',[200 200 1000 500]);
-
-for i = 1 : 5
+    figure;
+    set(gcf,'Position',[200 200 1000 500]);
     
-    subplot(2,5,i);
-
-    modelerr=modeltrue_tm - V_tm(:,i);
-    meanerr=mean(modelerr);
-    stderr=std(modelerr);
+    for i = 1 : 5
+        
+        subplot(2,5,i);
+        
+        modelerr=modeltrue_tm - V_tm(:,i);
+        meanerr=mean(modelerr);
+        stderr=std(modelerr);
+        
+        pdeplot(model,'XYData', modelerr')
+        
+        colormap jet
+        xlabel('x')
+        ylabel('y')
+        title(['TM_{', num2str(tm_mode_arr(i,1)),num2str(tm_mode_arr(i,2)),'}']);
+        
+    end
     
-    pdeplot(model,'XYData', modelerr')
-
-    colormap jet
-    xlabel('x')
-    ylabel('y')
-    title(['TM_{', num2str(tm_mode_arr(i,1)),num2str(tm_mode_arr(i,2)),'}']);
+    for i = 1 : 5
+        
+        subplot(2,5,i+5);
+        
+        modelerr=modeltrue_te - V_te(:,i);
+        meanerr=mean(modelerr);
+        stderr=std(modelerr);
+        
+        pdeplot(model,'XYData', modelerr')
+        
+        colormap jet
+        xlabel('x')
+        ylabel('y')
+        title(['TE_{', num2str(te_mode_arr(i,1)),num2str(te_mode_arr(i,2)),'}']);
+        
+    end
     
-end
-
-for i = 1 : 5
-    
-    subplot(2,5,i+5);
-
-    modelerr=modeltrue_te - V_te(:,i);
-    meanerr=mean(modelerr);
-    stderr=std(modelerr);
-    
-    pdeplot(model,'XYData', modelerr')
-
-    colormap jet
-    xlabel('x')
-    ylabel('y')
-    title(['TE_{', num2str(te_mode_arr(i,1)),num2str(te_mode_arr(i,2)),'}']);
-    
-end
-
 end
