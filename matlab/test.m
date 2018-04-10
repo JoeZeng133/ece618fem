9fa00fe3208b21e68a5e9c18f578ee1d118526f6
JoeZeng133 <35885015+JoeZeng133@users.noreply.github.com>
2018-04-09 22:19:11 -0400

    test modified

--- matlab/test.m
+++ matlab/test.m
@@ -23,9 +23,9 @@ geometryFromEdges(model, dl);
 % for 2nd order mesh, let 'GeometricOrder'='quadratic' and call quad.exe
 % for 1st order mesh, let 'GeometricOrder'='linear'    and call linear.exe
 if order == 1
-    mesh = generateMesh(model, 'Hmax', 0.1, 'GeometricOrder', 'linear'); %generate mesh with maximum element length 0.02
+    mesh = generateMesh(model, 'Hmax', em_size, 'GeometricOrder', 'linear'); %generate mesh with maximum element length 0.02
 else
-    mesh = generateMesh(model, 'Hmax', 0.1, 'GeometricOrder', 'quadratic'); %generate mesh with maximum element length 0.02
+    mesh = generateMesh(model, 'Hmax', em_size, 'GeometricOrder', 'quadratic'); %generate mesh with maximum element length 0.02
 end
 
 
@@ -92,59 +92,122 @@ B = sparse(Bi, Bj, Bv, numNode, numNode);
 fclose(fileID);
 
 %% TMz
-% solve generalized eigenvalue problem (A - lambda * B) * x = 0
-num_eig = 6;
-Aminusdir = A(~dirichlet, ~dirichlet);
-Bminusdir = B(~dirichlet, ~dirichlet);
-
-[eigfunc, eigval] = eigs(Aminusdir, Bminusdir, num_eig , 'SA'); %impose dirichlet boundary
-eigval = diag(eigval);
-
-V = zeros([numNode num_eig]);
-V(~dirichlet, :) = eigfunc;
-
-modefunc = @(m, n, x, y) sin(m * pi * x / a) .* sin(n * pi * y / b);
-% mode5 =  modefunc(4, 1, mesh.Nodes(1,:), mesh.Nodes(2, :))';
-% mode6 =  modefunc(2, 2, mesh.Nodes(1,:), mesh.Nodes(2, :))';
- 
-figure(2)
-title('TM Mode')
-for i = 1 : num_eig
-    subplot(2, 3,i)
-    mode = V(:, i);
-    mode = mode / max(abs(mode(:)));
-    pdeplot(model,'XYData', mode)
-    colormap jet
-    xlabel('x')
-    ylabel('y')
-    axis equal
-    axis([0 a 0 b])
-    title(['TM k= ', num2str(eigval(i))])
-end
+% num_eig = 5;
+% Aminusdir = A(~dirichlet, ~dirichlet);
+% Bminusdir = B(~dirichlet, ~dirichlet);
+% 
+% [eigfunc, eigval] = eigs(Aminusdir, Bminusdir, num_eig , 'SA'); %impose dirichlet boundary
+% eigval = diag(eigval);
+% 
+% V = zeros([numNode num_eig]);
+% V(~dirichlet, :) = eigfunc;
+% 
+% modefunc = @(m, n, x, y) sin(m * pi * x / a) .* sin(n * pi * y / b);
+% modes = zeros(size(V));
+% mn = [1 1; 2 1; 3 1; 1 2; 2 2];
+% 
+% for i = 1 : num_eig
+%     modes(:, i) = modefunc(mn(i, 1), mn(i, 2), xdata, ydata)';
+% end
+%  
+% figure(2)
+% 
+% diff = cell([num_eig 1]);
+% error = zeros([num_eig 1]);
+% 
+% for i = 1 : num_eig
+%     subplot(num_eig, 2, i * 2 - 1)
+%     mode = V(:, i);
+%     mode = mode / max(abs(mode(:)));
+%     pdeplot(model,'XYData', mode)
+%     colormap jet
+%     xlabel('x')
+%     ylabel('y')
+%     title(['Numerical TM ', 'm = ', num2str(mn(i, 1)), ', n = ', num2str(mn(i, 2))])
+%     
+%     subplot(num_eig, 2, i * 2)
+%     pdeplot(model,'XYData', modes(:, i))
+%     colormap jet
+%     xlabel('x')
+%     ylabel('y')
+%     title(['Analytical TM ', 'm = ', num2str(mn(i, 1)), ', n = ', num2str(mn(i, 2))])
+%     
+%     
+%     diff{i} = abs(mode) - abs(modes(:, i));
+%     error(i) = diff{i}' * B * diff{i};
+% end
+% 
+% disp(['The number of mesh is', num2str(em_size)])
+% fprintf('%.3e ', error)
+% fprintf('\n')
 
 
 %% TEz
-num_eig = 6;
+num_eig = 5;
 [eigfunc, eigval, flag] = eigs(A,B,num_eig + 1,'sa');
 
 V = eigfunc;
+Vn = zeros(size(V));
 eigval = diag(eigval);
 
-% modefunc = @(m, n, x, y) cos(m * pi * x / a) .* cos(n * pi * y / b);
-% mode2 =  modefunc(2, 0, mesh.Nodes(1,:), mesh.Nodes(2, :))';
-% mode3 =  modefunc(0, 1, mesh.Nodes(1,:), mesh.Nodes(2, :))';
+modefunc = @(m, n, x, y) cos(m * pi * x / a) .* cos(n * pi * y / b);
+modes = zeros(size(V));
+mn = [1 0; 0 1; 2 0; 1 1; 2 1];
+
+for i = 1 : num_eig
+    modes(:, i) = modefunc(mn(i, 1), mn(i, 2), xdata, ydata)';
+end
+ 
+diff = cell([num_eig 1]);
+error = zeros([num_eig 1]);
 
 figure(3)
 title('TE Mode')
 for i = 1 : num_eig
-    subplot(2, 3,i)
     mode = V(:, i + 1);
-    mode = (mode - min(mode)) / (max(mode) - min(mode)) * 2 - 1;
+    mode = mode / max(abs(mode(:)));
+    Vn(:, i + 1) = mode;
+
+    subplot(num_eig, 2, i * 2 - 1)
     pdeplot(model,'XYData', mode)
     colormap jet
     xlabel('x')
     ylabel('y')
-    axis equal
-    axis([0 a 0 b])
-    title(['TE k =  ', num2str(eigval(i + 1))])
+    title(['Numerical TE ', 'm = ', num2str(mn(i, 1)), ', n = ', num2str(mn(i, 2))])
+    
+    subplot(num_eig, 2, i * 2)
+    pdeplot(model,'XYData', modes(:, i))
+    colormap jet
+    xlabel('x')
+    ylabel('y')
+    title(['Analytical TE ', 'm = ', num2str(mn(i, 1)), ', n = ', num2str(mn(i, 2))])
+    
+    diff{i} = abs(mode) - abs(modes(:, i));
+    error(i) = diff{i}' * B * diff{i};
 end
+
+% figure(4)
+% for i = 2 : 3
+%     subplot(2, 2, i * 2 - 3)
+%     pdeplot(model,'XYData', Vn(:, i + 1))
+%     colormap jet
+%     xlabel('x')
+%     ylabel('y')
+%     title(['Numerical TE ', 'm = ', num2str(mn(i, 1)), ', n = ', num2str(mn(i, 2))])
+%     
+%     subplot(2, 2, i * 2 - 2)
+%     pdeplot(model,'XYData', modes(:, i))
+%     colormap jet
+%     xlabel('x')
+%     ylabel('y')
+%     title(['Analytical TE ', 'm = ', num2str(mn(i, 1)), ', n = ', num2str(mn(i, 2))])
+% end
+
+nu = Vn(:, 3:4);
+an = modes(:, 2 : 3);
+tr = an \ nu;
+
+
+disp(['The number of mesh is ', num2str(size(mesh.Elements, 2))])
+fprintf('%.2e ', error)
+fprintf('\n')
